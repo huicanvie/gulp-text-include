51f6926acc48d481ac1ced61e87ebf28bb20aa06
zjh <huikevin@aliyun.com>
2018-05-04 17:41:42 +0800

    fix

--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var include = require('gulp-html-include');
 
 gulp.task('include',function(){
-    return gulp.src('index.html')
+    return gulp.src('index.html',{buffer: false})
     .pipe(include())
     .pipe(gulp.dest('dist'));
 });
--- index.js
+++ index.js
@@ -9,39 +9,61 @@ const PLUGIN_NAME = 'gulp-html-include'
 function err (text) {
     return new PluginError(PLUGIN_NAME,text)
 }
-function findFile (path) {
-    
+function change2Stream (text) {
+    var stream = through();
+    stream.write(text);
+    return stream;
 }
 function htmlInclude() {
   let reg = /(<include[^<>]*>)/gi;
    
    /////
    let stream = through.obj(function(file, enc, cb) {
-       if (file.isStream()) {
-           this.emit('error', err('Streams are not supported!'))
-           return cb();
+        let base = file.base
+        
+       // console.log(file.contents.setEncoding('utf-8'))
+       if ( file.isStream()) {
+            let contents = ''
+            file.contents.setEncoding('utf8')
+            file.contents.on('data',(chunk) => {
+                contents += chunk
+            })
+            file.contents.on('end',(chunk) => {
+                let replaceTagArr = contents.match(reg)  ///["<include src="tt.html">"]
+                let pathArr = []
+                replaceTagArr.forEach(tag => {
+                    if (/src="(.*)"/gi.test(tag)){
+                        pathArr.push(RegExp.$1);
+                    }
+                });
+                console.log(pathArr)
+                ////find out the file
+                let newHtmlText = myFile(contents,base,pathArr)
+                let streamer = change2Stream(newHtmlText);
+                console.log(streamer)
+                console.log(newHtmlText)
+               streamer.on('error', this.emit.bind(this, 'error'));
+               file.contents = streamer;
+            })
+           
        }
 
        if (file.isBuffer()) {
-        //    file.contents = htmlTextBuffer
-       }
-       let base = file.base
-       let contents = file.contents.toString('utf8')
-       let replaceTagArr = contents.match(reg)  ///["<include src="tt.html">"]
-       let pathArr = []
+        let contents = file.contents.toString('utf8')
+        let replaceTagArr = contents.match(reg)  ///["<include src="tt.html">"]
+        let pathArr = []
 
-       //console.log(contents)
-       replaceTagArr.forEach(tag => {
-           if (/src="(.*)"/gi.test(tag)){
-               pathArr.push(RegExp.$1);
-           }
-       });
+        
+        replaceTagArr.forEach(tag => {
+            if (/src="(.*)"/gi.test(tag)){
+                pathArr.push(RegExp.$1);
+            }
+        });
        console.log(pathArr)
-
-       ////find out the file
-       let newHtmlText = myFile(contents,base,pathArr)
-
-       file.contents = new Buffer(newHtmlText)
+        ////find out the file
+        let newHtmlText = myFile(contents,base,pathArr)
+          file.contents = new Buffer(newHtmlText)
+       }
       
        this.push(file);
 
